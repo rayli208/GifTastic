be483341c82fe62f154e9323dc16e94beb94a311
Rayli Raykov <rayli208@gmail.com>
2018-12-22 18:22:11 -0500

    include user creation of memes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,30 @@
-console.log('Working');
+//TAKE USER INPUT
+var memes = [];
+
+function alertmemeName() {
+  var memeAlert = $(this).attr("data-name");
+  alert(memeAlert);
+}
+
+function renderButtons() {
+  $("#meme-name-holder").empty();
+  for (var i = 0; i < memes.length; i++) {
+    var a = $("<a>");
+    a.addClass("meme btn btn-warning btn-block text-white mb-2");
+    a.attr("data-name", memes[i]);
+    a.text(memes[i]);
+    $("#meme-name-holder").append(a);
+  }
+}
+
+$("#add-meme").on("click", function (event) {
+  event.preventDefault();
+  var meme = $("#user-meme-input").val().trim();
+  memes.push(meme);
+  renderButtons();
+});
+
+$(document).on("click", ".meme", alertmemeName);
+renderButtons();
+
+//DISPLAY ON SCREEN
